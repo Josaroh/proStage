2812087e8fda073940b397857ecfceb2a0152df9
Nathan Vierat Gouvet <nvgouvet@iutbayonne.univ-pau.fr>
2021-12-02 09:24:39 +0100

    toutes les routes sauf l'id

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -11,10 +11,45 @@ class ProStageController extends AbstractController
     /**
      * @Route("/", name="pro_stage_accueil")
      */
+
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'Controleur ProStage',
+            'controller_name' => 'Controleur ProStage accueil',
+        ]);
+    }
+
+    /**
+     * @Route("/entreprises", name="pro_stage_entreprises")
+     */
+
+    public function entreprises_vue(): Response
+    {
+        return $this->render('pro_stage/entreprises.html.twig', [
+            'controller_name' => 'Controleur ProStage entreprises',
+        ]);
+    }
+
+    /**
+     * @Route("/formations", name="pro_stage_formations")
+     */
+
+    public function foramtions_vue(): Response
+    {
+        return $this->render('pro_stage/formations.html.twig', [
+            'controller_name' => 'Controleur ProStage formations',
+        ]);
+    }
+
+    /**
+     * @Route("/stages/{id}", name="pro_stage_stages")
+     */
+
+    public function stages_vue($id): Response
+    {
+        return $this->render('pro_stage/stages.html.twig', [
+            'controller_name' => 'Controleur ProStage stages',
+            'idRessource' => $id
         ]);
     }
 }
